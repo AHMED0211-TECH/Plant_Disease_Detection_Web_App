c15921cf4575e4a2f1d86d24622eec74427fbd6e
Shuaib Ahmed <ShuaibAhmed0211@gamil.com>
2026-09-05 13:18:13 +0530

    switch to gunicorn production server

--- backend/App.py
+++ backend/App.py
@@ -123,6 +123,6 @@ def predict():
 # 🏁 RUN SERVER
 # ----------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7860, debug=True)
+    app.run(host="0.0.0.0", port=7860)
 #if __name__ == "__main__":
     #app.run(host="0.0.0.0", port=5001, debug=True)
